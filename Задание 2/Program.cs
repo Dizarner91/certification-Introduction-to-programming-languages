a1a6e1466bea15dd8fe117979196b6cb3640900a
Aleksandr Rytikov <9213940541@mail.ru>
2024-03-19 16:28:19 +0700

    добавлено решение 2 задания

---
+++ Задание 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ввод значений m и n
+        Console.Write("Введите значение m: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        // Вызов функции Аккермана и вывод результата
+        int result = AckermannFunction(m, n);
+        Console.WriteLine($"Результат функции Аккермана для m={m} и n={n} равен {result}");
+    }
+
+    static int AckermannFunction(int m, int n)
+    {
+        // Проверяем, если значения m и n превышают пороговые значения
+        if (m > 3)
+            return n + 1;
+        else if (m == 0)
+            return n + 1;
+        else if (n == 0)
+            return AckermannFunction(m - 1, 1);
+        else
+            return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+    }
+}
+
