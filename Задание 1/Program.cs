980971d0207027bff260ca1de822209ba30e6950
Aleksandr Rytikov <9213940541@mail.ru>
2024-03-19 16:26:28 +0700

    добавлено решение 1 задания

---
+++ Задание 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ввод значений M и N
+        Console.Write("Введите значение M: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        // Вызов рекурсивной функции для вывода чисел
+        PrintNaturalNumbers(m, n);
+    }
+
+    static void PrintNaturalNumbers(int m, int n)
+    {
+        // Базовый случай: если m больше n, то завершаем рекурсию
+        if (m > n)
+            return;
+
+        // Выводим текущее значение m
+        Console.WriteLine(m);
+
+        // Рекурсивный вызов функции с увеличенным на единицу m
+        PrintNaturalNumbers(m + 1, n);
+    }
+}
+
